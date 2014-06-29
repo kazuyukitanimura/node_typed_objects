ccc7d8df6d9a66f8479cdd62839b62c88cb55aa3
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-29 14:34:40 -0700

    test.js

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var assert = require("assert");
+var TypedObject = require('..');
+
+describe('TypedObject', function() {
+  var to = new TypedObject(1.0, {
+    a: 1.1,
+    'b': 1.2,
+    3: 1.3
+  });
+  to.A = 2.1;
+  to['B'] = 2.2;
+  describe('#Get', function() {
+    it('should to the expected value', function() {
+      assert.deepEqual(to.a, 1.1);
+      assert.deepEqual(to.b, 1.2);
+      assert.deepEqual(to[3], 1.3);
+      assert.deepEqual(to.A, 2.1);
+      assert.deepEqual(to.B, 2.2);
+      assert.deepEqual(to.C, 1.0); // default value
+    });
+  });
+});
