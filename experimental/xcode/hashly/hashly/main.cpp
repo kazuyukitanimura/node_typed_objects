72054abcb6b8d150c741c3d322b9ffaf268c154b
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-07-12 00:20:56 -0700

    unordered_map

--- experimental/xcode/hashly/hashly/main.cpp
+++ experimental/xcode/hashly/hashly/main.cpp
@@ -9,14 +9,17 @@
 #include <iostream>
 #include "hashly.h"
 #include <ctime>
+#include <unordered_map>
 
 #define START begin = std::clock()
-#define END(name) duration = 1000 * double(std::clock() - begin) / CLOCKS_PER_SEC; std::cout << (name) << ": " << duration << " milliseconds\n"
+#define END(name) duration = 1000 * 1000 * 1000 / size * double(std::clock() - begin) / CLOCKS_PER_SEC; std::cout << (name) << ": " << duration << " ns/op\n"
 
+typedef std::unordered_map<std::string, double > MapType;
+MapType map;
 
 int main(int argc, const char * argv[]) {
   // regression test...
-  std::cout << "Hello, World!\n";
+  std::cout << "Hashly performance test!\n";
   Hashly* h = new Hashly(0.0);
   uint32_t size = 10000;
   clock_t begin;
@@ -43,6 +46,28 @@ int main(int argc, const char * argv[]) {
   END("del");
 
   delete h;
+
+  std::cout << "\nstd::unordered_map performance test!\n";
+  START;
+  for (uint32_t i = size; i--;) {
+    map.insert(std::pair<std::string, double >(std::to_string(i), (double)i));
+  }
+  END("set");
+
+  START;
+  for (uint32_t i = size; i--;) {
+    auto itr = map.find(std::to_string(i));
+    if (itr->second != (double)i) {
+      std::cout << "actual: " << itr->second << ", expected: " << (double)i << "\n";
+    }
+  }
+  END("get");
+
+  START;
+  for (uint32_t i = size; i--;) {
+    map.erase(map.find(std::to_string(i)));
+  }
+  END("del");
 
   return 0;
 }
